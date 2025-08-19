d0721eae7d362e130a1b5163d8192732e9eee01a
NazarPolit <politovskyinazar@gmail.com>
2025-08-19 14:57:23 +0300

    PATCH added

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -22,42 +22,117 @@ namespace Booking_REST_API.Controllers
         public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
         {
             if (_context.Bookings == null)
-            {
                 return NotFound(new { message = "Not Found" });
-            }
+
             return await _context.Bookings.ToListAsync();
         }
 
+        //GET: api/Bookings/1
         [HttpGet("{id}")]
         public async Task<ActionResult<Booking>> GetBooking(int id)
         {
-            if(_context.Bookings == null)
-            {
+            if (_context.Bookings == null)
                 return NotFound(new { message = "Not Found" });
-            }
 
             var booking = await _context.Bookings.FindAsync(id);
 
-            if (booking == null) 
-            {
+            if (booking == null)
                 return NotFound(new { message = "Not Found" });
-            }
 
             return booking;
         }
 
-        [HttpPost("{id}")]
+        //POST: api/Bookings
+        [HttpPost]
         public async Task<ActionResult<Booking>> CreateBooking(Booking booking)
         {
             if (_context.Bookings == null)
-            {
                 return Problem("Entity set 'BookingsContext.Booking' is null.");
-            }
+
             _context.Bookings.Add(booking);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetBooking", new { id = booking.Id }, booking);
         }
 
+        //DELETE: api/Bookings/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBooking(int id)
+        {
+            if (_context.Bookings == null)
+                return NotFound(new { message = "Not Found" });
+
+            var booking = await _context.Bookings.FindAsync(id);
+
+            if (booking == null)
+                return NotFound(new { message = "Not Found" });
+
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Booking deleted." });
+        }
+
+        //PUT: api/Bookings/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBooking(int id, Booking booking)
+        {
+            if (id != booking.Id)
+                return BadRequest();
+
+            _context.Entry(booking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(booking.Id))
+                    return NotFound(new { message = "Not Found" });
+
+                else throw;
+            }
+
+            return Ok(new { message = "Booking updated successfully." });
+
+        }
+
+        //PATCH: api/Bookings/1
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<Booking>> PatchBooking(int id, Booking booking)
+        {
+            var existingBooking = await _context.Bookings.FindAsync(id);
+
+            if (existingBooking == null)
+                return NotFound(new { message = "Not Found" });
+            
+            if (booking.GuestName != null)
+                existingBooking.GuestName = booking.GuestName;
+
+            if (booking.RoomNumber != null)
+                existingBooking.RoomNumber = booking.RoomNumber;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(booking.Id))
+                    return NotFound(new { message = "Not Found" });
+
+                else throw;
+            }
+
+            return Ok(new { message = "Booking updated successfully." });
+
+        }
+        public bool BookingExists(int id)
+        {
+            return (_context.Bookings?.Any(b => b.Id == id)).GetValueOrDefault();
+        }
+        
     }
 }
